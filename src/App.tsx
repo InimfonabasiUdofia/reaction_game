f9317bb113109302338883dbb077f5d898eea30a
Inimfonabasi Udofia <137561774+InimfonabasiUdofia@users.noreply.github.com>
2025-09-01 11:49:31 +0100

    deleted css files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ReactionSpeedGame from './components/Reactionspeedgame'
 export default function App() { 
     return(
         <>
-        <div className=' lg:mx-[320px] md:mx-[50px]'>
+        <div className=' '>
             <ReactionSpeedGame />
         </div>
 
